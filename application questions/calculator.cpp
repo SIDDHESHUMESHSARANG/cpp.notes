7f641db0dc0f6ad606387dd2cdcc2d2e8c7d03a7
Siddhesh Umesh Sarang <siddheshsarang0811@gmail.com>
2024-12-16 21:04:29 +0530

    Q1

--- application questions/calculator.cpp
+++ application questions/calculator.cpp
@@ -1,1 +1,45 @@
+// This is a simple calculator made using C++
+// I used Visual Studio Code to write this program
+// The source code of this program is formatted by 'Prettier' code formator.
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    char operation;
+
+    cout << "Enter the First number: ";
+    cin >> num1;
+    cout << "Enter the Operator (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter the Second number: ";
+    cin >> num2;
+
+    switch (operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+
+        case '/':
+            if (num2 != 0)
+                cout << "Result: " << num1 / num2 << endl;
+            else
+                cout << "Cannot divide by Zero" << endl;
+            break;
+
+        default:
+            cout << "Invalid Operator" << endl;
+            break;
+    }
+
+    return 0;
+}
